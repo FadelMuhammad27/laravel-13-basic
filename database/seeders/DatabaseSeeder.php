19902700f7495a4b72ebe88271c1aefa79386fb3
Fadel Muhammad <fadel24@mhs.unitama.ac.id.com>
2026-04-23 20:45:21 +0800

    Pertemuan 5:Relationship

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 
-
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([StudentSeeder::class,]);
+        $this->call([
+            StudentSeeder::class,
+            DepartmentSeeder::class,
+            LecturerSeeder::class,
+            ]);
     }
 }
